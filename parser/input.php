36155b60a3c02ee9b4b6e4d492fe61840decedcb
salitygen <salitygen@yandex.ru>
2022-05-03 16:35:02 +0300

    autocommit

--- parser/input.php
+++ parser/input.php
@@ -3,7 +3,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
-header("Connection: close");
 
 if(isset($_POST['data'])){
 	$current = file_get_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json');
@@ -11,6 +10,6 @@ if(isset($_POST['data'])){
 	file_put_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json',str_replace('}][{','},{',$current));
 }
 
-print 'success';
+die('success');
 
 ?>
