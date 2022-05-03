b11130d4b408ea8f161d86bdc0ddee231120aea9
salitygen <salitygen@yandex.ru>
2022-05-03 17:55:01 +0300

    autocommit

--- parser/input.php
+++ parser/input.php
@@ -1,15 +1,23 @@
 <?php
+	include($_SERVER['DOCUMENT_ROOT'].'/parser/simple_html_dom.php');
+	$html = file_get_html('https://indexiq.ru/catalog/vse-smartfony/');
+?>
+<pre>
+<?php 
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+var_dump($html);
 
-if(isset($_POST['data'])){
-	$current = file_get_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json');
-	$current .= $_POST['data'];
-	file_put_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json',str_replace('}][{','},{',$current));
-}
 
-die('success');
+//header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+//header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+
+//if(isset($_POST['data'])){
+//	$current = file_get_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json');
+//	$current .= $_POST['data'];
+//	file_put_contents('/home/server/web/developer/electrohype.localh0st.ru/public_html/parser/parse/phones.json',str_replace('}][{','},{',$current));
+//}
+
+//die('success');
 
 ?>
