46cafc913c991a6e51f328fe366bce64d9db677d
salitygen <salitygen@yandex.ru>
2022-05-20 14:25:02 +0300

    autocommit

--- templates/enginevox/html/com_content/category/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
-?>
-<div class="blog home<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Blog">
-
-</div>
--- templates/enginevox/html/layouts/joomla/content/full_image.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('JPATH_BASE') or die;
-$params = $displayData->params;
-?>
-<?php $images = json_decode($displayData->images); ?>
-<?php if (!empty($images->image_fulltext)):?>
-	<img src="<?php echo htmlspecialchars($images->image_fulltext); ?>"> 
-<?php endif; ?> 
